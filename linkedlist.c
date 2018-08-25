683c98e57d4bbe3779fa61a91c6481bfacc79b85
Aditya Gaitonde <gaitondeaditya@gmail.com>
2018-08-25 22:26:36 +0530

    commented linked list code

--- linkedlist.c
+++ linkedlist.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>//for free() and malloc()
 
 struct node{
     int data;
     struct node *link;
 };
 
-void display(struct node *q){
+void display(struct node *q){//function to display all nodes in the linked list
     //struct node *q;
     printf("\nthe linked list is:\n" );
     for(;q!=NULL;q=q->link)
@@ -14,46 +14,47 @@ void display(struct node *q){
     printf("\n" );
 }
 
-void append(int x,struct node **head){
+void append(int x,struct node **head){//function to add node at the end
     struct node *p ,*q;
     q=(struct node *)malloc(sizeof(struct node));
-    if(*head==NULL){
-        //printf("hello\n" );
-
-        *head=q;}
-    else{
-        for(p=*head;p->link!=NULL;p=p->link);
+    if(*head==NULL){//this works the node being added is first node
+        *head=q;
+    }
+    else{//this works in other case
+        for(p=*head;p->link!=NULL;p=p->link);//for loop to go to the end if the linked list
         p->link=q;
 
     }
-    q->data=x;
-    q->link=NULL;
+    q->data=x;//assigns value to data part of the node
+    q->link=NULL;//makes the link NULL
     //display(*head);
 }
 
-int size(struct node *head){
+int size(struct node *head){//function to find size of the linked list
     int i=0;
     struct node *p;
     for(p=head;p!=NULL;p=p->link,i++);
     return i;
 
 }
-void addf(int x,struct node **head){
+void addf(int x,struct node **head){//function to add node at the beginning
     struct node *newnode;
     newnode=(struct node *)malloc(sizeof(struct node));
     newnode->data=x;
-    newnode->link=*head;
-    *head=newnode;
+    newnode->link=*head;//points the link part of newly created
+    //node to the first node of existing linked list
+    *head=newnode;//makes the newly created node first node
     //display(*head);
 }
 
-void addm(int pos,int x,struct node **head){
+void addm(int pos,int x,struct node **head){//function to add node at nth position from front
     int i;
     struct node *p,*newnode;
     newnode=(struct node *)malloc(sizeof(struct node));
     newnode->data=x;
 //    pos--;
-    if(pos>size(*head)){
+    if(pos>size(*head)){//comdition to check if the position is valid.
+        //the function returns control to main if linked list is too small
         printf("link list too small\n" );
         return;
     }
@@ -65,13 +66,14 @@ void addm(int pos,int x,struct node **head){
 
 
 }
-void  deletev(int val,struct node **head) {
+void  deletev(int val,struct node **head) {//finction to delete a value in the linked list
     struct node *p,*q;
-    while ((*head) -> data==val) {
+    while ((*head) -> data==val) {//works if the value passed is at the first node
         p=*head;
         *head=p->link;
-        free(p);}
-    for(p=*head;p->link!=NULL;p=p->link){
+        free(p);
+    }
+    for(p=*head;p->link!=NULL;p=p->link){//checks other nodes
         if(p->link->data==val){
             q=p->link;
             p->link=q->link;
